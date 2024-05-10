5c68a96b3560193375171e0aadde8890fde5c3ec
Császár Dániel <csaszardaniel81@gmail.com>
2024-05-10 22:43:01 +0200

    update

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,11 +7,21 @@ vim.g.mapleader = " "
 vim.opt.swapfile = false
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
-vim.keymap.set('n', '<leader>é', '$') 
+vim.keymap.set('n', '<leader>q',':w<CR>:Ex<CR>')
+vim.keymap.set('n', '<leader>é', '$')
 vim.keymap.set('n', 'j', 'h')
 vim.keymap.set('n', 'k', 'j')
 vim.keymap.set('n', 'l', 'k')
 vim.keymap.set('n', 'é', 'l')
+vim.keymap.set('v', 'j', 'h')
+vim.keymap.set('v', 'k', 'j')
+vim.keymap.set('v', 'l', 'k')
+vim.keymap.set('v', 'é', 'l')
+vim.keymap.set('n', '<leader>h', ':wq<CR>')
+
+-- netrw
+vim.cmd("autocmd FileType netrw nmap <buffer> <Tab> <CR>")
+
+-- else
 vim.wo.number = true
 vim.g.netrw_list_hide = ''
